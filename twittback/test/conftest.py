46289ad7c8fc5d7e1bff4c1c83c277fa997a8abd
Dimitri Merejkowsky <d.merej@gmail.com>
2017-04-17 16:22:02 +0200

    import ui

--- twittback/test/conftest.py
+++ twittback/test/conftest.py
@@ -1,5 +1,8 @@
+import re
+
 import path
 
+from twittback import ui
 import twittback
 import twittback.server
 
@@ -14,6 +17,31 @@ def app():
 @pytest.fixture
 def tmp_path(tmpdir):
     return path.Path(str(tmpdir))
+
+
+class MessageRecorder():
+    def __init__(self):
+        ui.CONFIG["record"] = True
+        ui._MESSAGES = list()
+
+    def stop(self):
+        ui.CONFIG["record"] = False
+        ui._MESSAGES = list()
+
+    def reset(self):
+        ui._MESSAGES = list()
+
+    def find(self, pattern):
+        regexp = re.compile(pattern)
+        for message in ui._MESSAGES:
+            if re.search(regexp, message):
+                return message
+
+@pytest.fixture()
+def messages(request):
+    recorder = MessageRecorder()
+    request.addfinalizer(recorder.stop)
+    return recorder
 
 
 class TweetFactory:
