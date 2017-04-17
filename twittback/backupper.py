3956aa9a0091165eb560a4fb398eea6cf40554f4
Dimitri Merejkowsky <d.merej@gmail.com>
2017-04-17 16:29:59 +0200

    Add some messages

--- twittback/backupper.py
+++ twittback/backupper.py
@@ -1,3 +1,6 @@
+from twittback import ui
+
+
 class Backupper:
     def __init__(self, *, client, storage):
         self.client = client
@@ -5,14 +8,21 @@ class Backupper:
 
     def yield_latest_tweets(self):
         latest_tweet = self.storage.latest_tweet()
-        for tweet in self.client.get_latest_tweets():
+        ui.info("Feching latest tweets", ui.ellipsis)
+        tweets = self.client.get_latest_tweets()
+        for tweet in tweets:
             if latest_tweet and (tweet.twitter_id <= latest_tweet.twitter_id):
                     break
             yield tweet
 
     def backup(self):
         latest_tweets = self.yield_latest_tweets()
-        self.storage.add(reversed(list(latest_tweets)))
+        to_add = list(latest_tweets)
+        self.storage.add(reversed(to_add))
+        if to_add:
+            ui.info("Stored", len(to_add), "new tweet(s)", ui.check)
+        else:
+            ui.info("No new tweets", ui.check)
 
 
 def main():
